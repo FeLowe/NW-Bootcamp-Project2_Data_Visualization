32afb142bc21eb012fb34971d51acc7a4e55ce59
A Deschenes <ameliadeschenes@AMELIAs-MacBook-Pro.local>
2019-06-24 19:57:37 -0500

    adding distance to API

--- Group App/static/js/map.js
+++ Group App/static/js/map.js
@@ -19,30 +19,28 @@ function buildPlot() {
     var data = response;
 
     for(row in data){
-      city.push(row[0]);
-      country.push(row[1]);
+      // city.push(row[0]);
+      // country.push(row[1]);
       lat.push(row[2]);
       lon.push(row[3]);
-      price.push(row[4]);
-      room_id.push(row[5]);
-      room_type.push(row[6]);
-      selection.push(row[7]);
+      // price.push(row[4]);
+      // room_id.push(row[5]);
+      // room_type.push(row[6]);
+      // selection.push(row[7]);
     };
 
-     
-
-
+    
      data = {
          type: "scattergeo",
          locationmode: "USA-states",
-         city: city,
-         country: country,
+        //  city: city,
+        //  country: country,
          lat: lat,
          lon: lon,
-         price: price,
-         room_id,
-         room_type,
-         selection,
+        //  price: price,
+        //  room_id,
+        //  room_type,
+        //  selection,
          hoverinfo: "text",
          marker: {
              "size": 50,
